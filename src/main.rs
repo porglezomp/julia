2d7c382f997d10c26f68b8f91537338dd4116dff
Caleb Jones <code@calebjones.net>
2018-04-12 17:20:54 -0400

    Rearrange imports (thanks new rustfmt)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate image;
 extern crate sdl2;
 
-use sdl2::render::Renderer;
 use sdl2::event::Event;
-use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode;
-use std::sync::mpsc::{channel, Sender};
-use std::thread;
+use sdl2::rect::Rect;
+use sdl2::render::Renderer;
 use std::cmp::{max, min};
 use std::io::Write;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
 
 use batches::{screen_rects, Batch, BATCH_HEIGHT, BATCH_WIDTH};
 
